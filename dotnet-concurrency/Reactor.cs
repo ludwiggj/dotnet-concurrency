b51b83d4bc773cd270e378f7ca33426c034db1d7
Graeme Ludwig <graeme.ludwig@sessioncam.com>
2020-12-11 18:56:01 +0000

    Initial version

---
+++ dotnet-concurrency/Reactor.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace dotnet_concurrency
+{
+	class Reactor
+	{
+		public static class ReactorEventDispatcher
+		{
+			private static readonly object eventDispatcherSyncObj = new object();
+
+			private static event Action startingUp;
+			private static event Action heatCritical;
+			private static event Action safetyShutdown;
+
+			public static event Action StartingUp
+			{
+				add
+				{
+					lock (eventDispatcherSyncObj)
+					{
+						startingUp += value;
+					}
+				}
+				remove
+				{
+					lock (eventDispatcherSyncObj)
+					{
+						startingUp -= value;
+					}
+				}
+			}
+
+			public static event Action HeatCritical
+			{
+				add
+				{
+					lock (eventDispatcherSyncObj)
+					{
+						heatCritical += value;
+					}
+				}
+				remove
+				{
+					lock (eventDispatcherSyncObj)
+					{
+						heatCritical -= value;
+					}
+				}
+			}
+
+			public static event Action SafetyShutdown
+			{
+				add
+				{
+					lock (eventDispatcherSyncObj)
+					{
+						safetyShutdown += value;
+					}
+				}
+				remove
+				{
+					lock (eventDispatcherSyncObj)
+					{
+						safetyShutdown -= value;
+					}
+				}
+			}
+
+			internal static void TriggerStartUp()
+			{
+				lock (eventDispatcherSyncObj)
+				{
+					if (startingUp != null) startingUp();
+				}
+			}
+
+			internal static void TriggerHeatCritical()
+			{
+				lock (eventDispatcherSyncObj)
+				{
+					if (heatCritical != null) heatCritical();
+				}
+			}
+
+			internal static void TriggerSafetyShutdown()
+			{
+				lock (eventDispatcherSyncObj)
+				{
+					if (safetyShutdown != null) safetyShutdown();
+				}
+			}
+
+			internal static void TriggerSafetyShutdownSafe()
+			{
+				Action safetyShutdownLocal;
+				lock (eventDispatcherSyncObj)
+				{
+					safetyShutdownLocal = safetyShutdown;
+				}
+				if (safetyShutdownLocal != null) safetyShutdownLocal();
+			}
+		}
+
+		static void Main(string[] args)
+		{
+			ReactorEventDispatcher.HeatCritical += () =>
+			{
+				var triggerShutdownTask = Task.Run((Action)ReactorEventDispatcher.TriggerSafetyShutdown);
+
+				// OtherLongRunningSafetyCode();
+
+				triggerShutdownTask.Wait();
+			};
+		}
+	}
+}
---
+++ dotnet-concurrency/WordCount1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace dotnet_concurrency
+{
+    class WordCount1
+    {
+		private static List<string> wordList;
+		private static List<string> curseWords;
+		private static int currentWordIndex;
+		private static Dictionary<string, int> wordCountDict;
+
+		public static void CalculateWordCounts()
+		{
+			wordList = new List<string> { "the", "lazy", "fox", "jumped", "over", "the", "brown", "dog", "and", "kissed", "the", "cock", "bum" };
+			curseWords = new List<string> { "bum", "cock" };
+			currentWordIndex = 0;
+			wordCountDict = new Dictionary<string, int>();
+
+			Thread threadA = new Thread(ThreadDoWork);
+			Thread threadB = new Thread(ThreadDoWork);
+			Thread threadC = new Thread(ThreadDoWork);
+			Thread threadD = new Thread(ThreadDoWork);
+			threadA.Start();
+			threadB.Start();
+			threadC.Start();
+			threadD.Start();
+			threadA.Join();
+			threadB.Join();
+			threadC.Join();
+			threadD.Join();
+		}
+
+		private static void ThreadDoWork()
+		{
+			bool atLeastOneWordRemaining;
+			int thisWordIndex = currentWordIndex;
+			currentWordIndex = currentWordIndex + 1;
+			if (thisWordIndex >= wordList.Count) atLeastOneWordRemaining = false;
+			else atLeastOneWordRemaining = true;
+
+			while (atLeastOneWordRemaining)
+			{
+				string thisWord = wordList[thisWordIndex];
+				bool firstOccurrenceOfWord = !wordCountDict.ContainsKey(thisWord);
+
+				if (curseWords.Contains(thisWord)) Console.WriteLine("Curse word detected!");
+				if (firstOccurrenceOfWord) wordCountDict.Add(thisWord, 1);
+				else wordCountDict[thisWord] = wordCountDict[thisWord] + 1;
+
+				thisWordIndex = currentWordIndex;
+				currentWordIndex = currentWordIndex + 1;
+				if (thisWordIndex >= wordList.Count) atLeastOneWordRemaining = false;
+			}
+		}
+		static void Main(string[] args)
+        {
+			CalculateWordCounts();
+
+			foreach (KeyValuePair<string, int> kv in wordCountDict)
+				Console.WriteLine("Key [" + kv.Key.ToString() + "] Value [" + kv.Value.ToString() + "]");
+		}
+    }
+}
